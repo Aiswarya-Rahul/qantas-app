3bad07d75097c237a8a4dbac1b62c006baef32a3
Aiswarya-Rahul <aiswaryapmohan.mec@gmail.com>
2025-02-12 15:43:30 +1100

    Linting and Formatting

--- eslint.config.js
+++ eslint.config.js
@@ -34,5 +34,11 @@ export default [
         { allowConstantExport: true },
       ],
     },
+    extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier" // Ensures Prettier rules override ESLint
+  ],
   },
 ]
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-function App() {
-  return <>App</>;
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
